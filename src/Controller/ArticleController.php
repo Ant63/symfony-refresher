e1064a67128205898fb3a605a9b5db40edd76ace
Anthony Averill <anthonyaverill1963@gmail.com>
2018-10-06 15:50:14 +0100

    Twig templates

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,10 +9,11 @@
 namespace App\Controller;
 
 //use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController {
+class ArticleController extends AbstractController {
 
   /**
    * @\Symfony\Component\Routing\Annotation\Route()
@@ -28,10 +29,22 @@ class ArticleController {
    */
   public function show($slug) {
 
-    return new Response(sprintf(
-      'this the page: %s',
-      $slug
-      )
+    $comments = [
+      'Credibly recaptiualize accurate mindshare without inexpensive human capital.',
+      'Dramatically e-enable exceptional solutions after wireless methods of empowerment.',
+      'Synergistically disseminate orthogonal processes through top-line schemas.'
+    ];
+
+    return $this->render(
+      'article/show.html.twig',
+      [
+        'title' => ucwords(str_replace('-', ' ', $slug)),
+        'content' => 'Synergistically brand progressive internal or "organic" 
+        sources for quality meta-services. Monotonectally fabricate user 
+        friendly information vis-a-vis progressive results. Seamlessly 
+        maintain visionary systems via one-to-one experiences.',
+        'comments' => $comments
+      ]
     );
 
   }
